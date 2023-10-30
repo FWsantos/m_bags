11d2f842608545f2421f893a763e34cdd839495a
FWsantos <fwsantos23@gmail.com>
2023-10-30 16:57:57 -0300

    reading api and list data

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,32 +1,46 @@
 import './App.css'
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react'
+
+interface Author {
+  id: number;
+  email: string;
+  name: string;
+}
+
+interface DataItem {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  title: string;
+  content: string;
+  published: boolean;
+  view_count: number;
+  author_id: number;
+  author: Author;
+}
 
 function App() {
-  const [data, setData] = useState(null);
-
-  function callApi() {
-    fetch("http://localhost:3000/feed")
-      .then(res => console.log(res))
-      // .then(res => {
-      //   setData(res);
-      //   console.log(data);
-      // })
-      .catch(err => console.log(err))
-  }
+  const [data, setData] = useState<DataItem[]>([])
 
   useEffect(() => {
-    callApi();
-    return () => {
-      console.log("Unmounting...");
-    }
-  }, []);
+    fetch('http://localhost:3000/feed')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error))
+  }, [])
 
   return (
     <>
-      <div>
-        <p>Olá Mundo!</p>
-        {/* <div>{data}</div> */}
-      </div>
+      <p>List of recomendations</p>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <h3>{item.author.name}</h3>
+            <p>{item.content}</p>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
